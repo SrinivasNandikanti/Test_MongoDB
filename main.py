51eb77e3a2c759e3ac8aebf4d9c739aebfa3f0ed
Srinivas_Nandikanti <sri_mme1194@outlook.com>
2022-07-20 19:29:03 +0530

    This is my first push to github

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pymongo
 client = pymongo.MongoClient("mongodb+srv://srinivas:admin123@cluster0.cea5rsa.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
+
 print(db)
 
-d = {"name":"srinivas",
-     "surname":"nandikanti",
-     "age": 28}
+d = {
+    "name":"Srinivas",
+    "lastname":"Nandikanti",
+    "DOB":"24081994"
+}
 
-db1 = client['mongotest']
+db1 = client["mongotest"]
 coll = db1['test']
-coll.insert_one(d )
+coll.insert_one(d)
+
